4653da891873defd1a7fb739f14d06d6fcd89ec5
evanjfarmer <evanjfarmer@gmail.com>
2016-09-18 10:42:12 -0400

    Added class to make front page image full size, deleted unnecessary code in content-home template

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -1,13 +1,8 @@
 <?php
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<img class="green-leaf" src="http://www.cannabisismyreligion.dev/wp-content/uploads/2016/09/green-leaf.jpg" />
 	<div class="entry-content">
-		<img src="http://www.cannabisismyreligion.dev/wp-content/uploads/2016/09/green-leaf.jpg" />
-		<div class="row">
-			<div class="call small-12 large-12 columns">
-				<h2></h2>
-			</div>
-		</div>
 		<div class="row">
 			<div class="intro small-12 medium-4 large-4 text-center columns">
 				<div class="faithimg"><img src="http://www.cannabisismyreligion.dev/wp-content/uploads/2016/09/love-leaf-icon.png" alt="Love Leaf" width="300" height="300" />
